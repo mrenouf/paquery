290bb42b09889413ba065ee4fb83a48b1b951c04
Mark Renouf <mark.renouf@gmail.com>
2010-03-22 09:18:06 -0400

    paquery.c: Added sink-input, source-output and module. Added -n option.

--- src/paquery.c
+++ src/paquery.c
@@ -3,41 +3,60 @@
 #include <pulse/pulseaudio.h>
 
 int verbose = 0;
+int numeric = 0;
 const char *APP_NAME = "paquery";
 
-void check_and_display(const char* name, pa_proplist *proplist, GList *properties) {
+void check_and_display(const char* name, int index, pa_proplist *proplist, GList *properties) {
   if (match(proplist, properties)) {
     // result to stdout
     if (verbose) {
-      printf("[%s]\n", name);
+      printf("[name=\"%s\", index=\"%d\"]\n", name, index);
       printf("%s\n", pa_proplist_to_string(proplist));
-    }
-    else {
-      printf("%s\n", name);
+    } else {
+      if (numeric)
+        printf("%d\n", index);
+      else
+        printf("%s\n", name);
     }
   }
 }
 
 static void pa_card_info_cb(pa_context *c, const pa_card_info*i, int eol, void *userdata) {
   if (!eol)
-    check_and_display(i->name, i->proplist, (GList*) userdata);
+    check_and_display(i->name, i->index, i->proplist, (GList*) userdata);
 }
 
 static void pa_sink_info_cb(pa_context *c, const pa_sink_info *i, int eol, void *userdata) {
   if (!eol)
-    check_and_display(i->name, i->proplist, (GList*) userdata);
+    check_and_display(i->name, i->index, i->proplist, (GList*) userdata);
 }
 
 static void pa_source_info_cb(pa_context *c, const pa_source_info *i, int eol, void *userdata) {
   if (!eol)
-    check_and_display(i->name, i->proplist, (GList*) userdata);
+    check_and_display(i->name, i->index, i->proplist, (GList*) userdata);
 }
 
 static void pa_client_info_cb(pa_context *c, const pa_client_info*i, int eol, void *userdata) {
   if (!eol)
-    check_and_display(i->name, i->proplist, (GList*) userdata);
+    check_and_display(i->name, i->index, i->proplist, (GList*) userdata);
 }
 
+static void pa_sink_input_info_cb(pa_context *c, const pa_sink_input_info *i, int eol,
+        void *userdata) {
+  if (!eol)
+    check_and_display(i->name, i->index, i->proplist, (GList*) userdata);
+}
+
+static void pa_source_output_info_cb(pa_context *c, const pa_source_output_info *i, int eol,
+        void *userdata) {
+  if (!eol)
+    check_and_display(i->name, i->index, i->proplist, (GList*) userdata);
+}
+
+static void pa_module_info_cb(pa_context *c, const pa_module_info *i, int eol, void *userdata) {
+  if (!eol)
+    check_and_display(i->name, i->index, i->proplist, (GList*) userdata);
+}
 
 typedef struct {
   const char *name;
@@ -59,21 +78,28 @@ int match(pa_proplist *proplist, GList *properties) {
 }
 
 enum QueryType {
+  QUERY_TYPE_MODULE,
+  QUERY_TYPE_CLIENT,
   QUERY_TYPE_CARD,
   QUERY_TYPE_SINK,
   QUERY_TYPE_SOURCE,
-  QUERY_TYPE_CLIENT
+  QUERY_TYPE_SINK_INPUT,
+  QUERY_TYPE_SOURCE_OUTPUT
 };
 
 void usage() {
   fprintf(stderr, "\nUsage: %s [options] [query_type] [<propery=value> ...]\n\n", APP_NAME);
   fprintf(stderr, "Options:\n");
-  fprintf(stderr, "    -v       Verbose: list all properties for each match\n\n");
+  fprintf(stderr, "    -v       Verbose: list all properties for each match\n");
+  fprintf(stderr, "    -n       Numeric: print indexes instead of names\n\n");
   fprintf(stderr, "Query Type (exactly one required):\n");
-  fprintf(stderr, "    card     List name of matching cards\n");
-  fprintf(stderr, "    sink     List name of matching sinks\n");
-  fprintf(stderr, "    source   List name of matching sources\n");
-  fprintf(stderr, "    client   List name of matching clients\n\n");
+  fprintf(stderr, "    module          List matching modules\n");
+  fprintf(stderr, "    client          List matching clients\n");
+  fprintf(stderr, "    card            List matching cards\n");
+  fprintf(stderr, "    sink            List matching sinks\n");
+  fprintf(stderr, "    source          List matching sources\n");
+  fprintf(stderr, "    sink-input      List matching sink-inputs\n\n");
+  fprintf(stderr, "    source-output   List matching source-outputs\n");
   fprintf(stderr, "The remainder of the arguments are taken as name-value pairs\n");
   fprintf(stderr, "which must be separated by an equals sign. If the value contains,\n");
   fprintf(stderr, "spaces, it must be quoted to be considered a single value.\n\n");
@@ -111,23 +137,34 @@ int main(int argc, char **argv) {
   while (i < argc && type == -1) {
     char *s = argv[i];
     if (s[0] == '-') {
-      if (s[1] == 'v') {
+      switch (s[1]) {
+      case 'v':
         verbose = 1;
-      } else {
+        break;
+      case 'n':
+        numeric = 1;
+        break;
+      default:
         fprintf(stderr, "Error: unknown option (%s)\n", s);
         usage();
         return EXIT_FAILURE;
       }
     } else {
       // The first non-option arg is taken as the query type
-      if (g_str_equal(s, "card")) {
+      if (g_str_equal(s, "module")) {
+        type = QUERY_TYPE_MODULE;
+      } else if (g_str_equal(s, "client")) {
+        type = QUERY_TYPE_CLIENT;
+      } else if (g_str_equal(s, "card")) {
         type = QUERY_TYPE_CARD;
       } else if (g_str_equal(s, "sink")) {
         type = QUERY_TYPE_SINK;
       } else if (g_str_equal(s, "source")) {
         type = QUERY_TYPE_SOURCE;
-      } else if (g_str_equal(s, "client")) {
-        type = QUERY_TYPE_CLIENT;
+      } else if (g_str_equal(s, "sink-input")) {
+        type = QUERY_TYPE_SINK_INPUT;
+      } else if (g_str_equal(s, "source-output")) {
+        type = QUERY_TYPE_SOURCE_OUTPUT;
       } else {
         fprintf(stderr, "Error! Invalid query type (%s), must be one of [card, sink, source]\n", s);
         usage();
@@ -183,6 +220,15 @@ int main(int argc, char **argv) {
     break;
   case QUERY_TYPE_CLIENT:
     op = pa_context_get_client_info_list(c, pa_client_info_cb, properties);
+    break;
+  case QUERY_TYPE_SINK_INPUT:
+    op = pa_context_get_sink_input_info_list(c, pa_sink_input_info_cb, properties);
+    break;
+  case QUERY_TYPE_SOURCE_OUTPUT:
+    op = pa_context_get_source_output_info_list(c, pa_source_output_info_cb, properties);
+    break;
+  case QUERY_TYPE_MODULE:
+    op = pa_context_get_module_info_list(c, pa_module_info_cb, properties);
     break;
   }
 
